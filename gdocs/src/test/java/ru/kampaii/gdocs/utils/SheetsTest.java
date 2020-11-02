9de9b051a043b0afa6ac35c9400dfd5760c8b2d5
Kuznetsov-KS <kuznetsov-KS@intech.rshb.ru>
2020-11-02 17:23:42 +0500

    Переделывает тест на спринг

--- gdocs/src/test/java/ru/kampaii/gdocs/utils/SheetsTest.java
+++ gdocs/src/test/java/ru/kampaii/gdocs/utils/SheetsTest.java
@@ -1,26 +1,29 @@
 package ru.kampaii.gdocs.utils;
 
-import com.google.api.services.sheets.v4.Sheets;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.kampaii.gdocs.config.GDocsConfiguration;
+import ru.kampaii.gdocs.services.GoogleSheetsService;
 
 import java.io.IOException;
-import java.security.GeneralSecurityException;
 import java.util.List;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = GDocsConfiguration.class)
 public class SheetsTest {
 
-    private static Sheets sheetsService;
+    @Autowired
+    private GoogleSheetsService service;
+
     private static String SPREADSHEET_ID = "1lwQDTx_DQPogr_bgBuPbD8Yqtk551Kkn-LKXPAUx7fc";
 
-    @BeforeClass
-    public static void setup() throws GeneralSecurityException, IOException {
-        sheetsService = SheetsServiceUtil.getSheetsService();
-    }
-
     @Test
-    public void whenWriteSheet_thenReadSheetOk() throws IOException, GeneralSecurityException {
-        List<List<Object>> values = sheetsService.spreadsheets().values().get(SPREADSHEET_ID, "A:F").execute().getValues();
+    public void whenWriteSheet_thenReadSheetOk() throws IOException{
+
+        List<List<Object>> values = service.getSheetValue(SPREADSHEET_ID);
 
         values.forEach(value -> soutValue(value));
 
